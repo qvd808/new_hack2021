3fe67b56fb3a3ef08e34092668b7bcd9b3607c6c
mathewtse <88808907+mathewtse@users.noreply.github.com>
2021-11-06 10:45:02 -0700

    mathew

--- background.js
+++ background.js
@@ -1,6 +1,3 @@
-// create a dictionary, save to chrome local storage
-
-// check which website the user is on every second
 
 // if the user is on a new website, add that website to the dictionary
 // make sure only the first part of the URL is saved. 
@@ -12,4 +9,15 @@
 // then, after the user switches tabs, it will do the currentTime - firstTime, and add that time to the dictionary
 
 
+// create a dictionary, save to chrome local storage
+
+var dict = { };
+
+// check which website the user is on every second
+
+chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+   function(tabs){  
+      alert(tabs[0].url);
+   }
+);
 
