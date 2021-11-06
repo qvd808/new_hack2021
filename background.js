95c2dce077e5c8d3868eceec479e53878a90554d
mathewtse <88808907+mathewtse@users.noreply.github.com>
2021-11-06 11:14:28 -0700

    k

--- background.js
+++ background.js
@@ -18,8 +18,7 @@ var dict = { };
 chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
    function(tabs){  
       // alert(tabs[0].url);
-      
-
+      console.log(tabs[0].url);
 
    }
 );
