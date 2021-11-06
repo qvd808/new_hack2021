37aef9148d3f74a21295054c9c924eca59a50152
mathewtse <88808907+mathewtse@users.noreply.github.com>
2021-11-06 10:17:01 -0700

    updated by mathew

--- background.js
+++ background.js
@@ -1,1 +1,15 @@
-alert("This is testing") //testing if we can upload it to git
+// create a dictionary, save to chrome local storage
+
+// check which website the user is on every second
+
+// if the user is on a new website, add that website to the dictionary
+// make sure only the first part of the URL is saved. 
+
+// if the user is on a website they already visited, add time to that website 
+
+// create a variable called firstTime on chrome local storage to keep track of the first time they open their active tab
+
+// then, after the user switches tabs, it will do the currentTime - firstTime, and add that time to the dictionary
+
+
+
